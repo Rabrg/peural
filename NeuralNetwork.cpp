7983bebd4ee518635ff202c47e2ee20951361135
Ryan Greene <rabrg96@gmail.com>
2018-10-29 12:09:49 -0500

    Removed redundant return of instance in addLayer

--- NeuralNetwork.cpp
+++ NeuralNetwork.cpp
@@ -1,11 +1,9 @@
 #include "NeuralNetwork.h"
 
-NeuralNetwork NeuralNetwork::addNeuralLayer(int layer_size, int (*activation)(Neuron)) {
+void NeuralNetwork::addNeuralLayer(int layer_size, int (*activation)(Neuron)) {
     layers.push_back(*new NeuralLayer(layers.back().layer_size, layer_size, activation));
-    return *this;
 }
 
-NeuralNetwork NeuralNetwork::addNeuralLayer(int input_size, int layer_size, int (*activation)(Neuron)) {
+void NeuralNetwork::addNeuralLayer(int input_size, int layer_size, int (*activation)(Neuron)) {
     layers.push_back(*new NeuralLayer(input_size, layer_size, activation));
-    return *this;
 }
--- NeuralNetwork.h
+++ NeuralNetwork.h
@@ -9,9 +9,9 @@ class NeuralNetwork {
 public:
     std::vector<NeuralLayer> layers;
 
-    NeuralNetwork addNeuralLayer(int layer_size, int (*activation)(Neuron));
+    void addNeuralLayer(int layer_size, int (*activation)(Neuron));
 
-    NeuralNetwork addNeuralLayer(int input_size, int layer_size, int (*activation)(Neuron));
+    void addNeuralLayer(int input_size, int layer_size, int (*activation)(Neuron));
 };
 
 
